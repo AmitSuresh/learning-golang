0f8c6b2b3cf173e7f3ac67efc0062c4be377b071
Amit Suresh <amit_suresh@outlook.com>
2023-02-10 03:38:49 +0530

    before_last_section

---
+++ 39-test/2mySum_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestMySumStruc(t *testing.T) {
+	type test1 struct {
+		data   []int
+		answer int
+	}
+
+	tests := []test1{
+		{[]int{1, 1}, 2},
+		{[]int{5, 5}, 10},
+		{[]int{10, 10}, 20},
+	}
+	for _, v := range tests {
+		got := mySum(v.data...)
+		if got != v.answer {
+			t.Errorf("Got: %v, Want: %v", got, v.answer)
+		}
+	}
+}
---
+++ 39-test/go.mod
@@ -0,0 +1,3 @@
+module test.com/mySum
+
+go 1.20
